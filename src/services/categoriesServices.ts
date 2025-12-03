cf608520934ab31e173871405a3c26b93f4f0f13
Aiham Jassar <AihamJassar@gmail.com>
2025-12-03 20:21:27 +0300

    feat: implement category management service with CRUD operations

---
+++ src/services/categoriesServices.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { CategorySchema } from "@/types/schema";
+import prisma from "@/lib/prisma";
+import { executeAction } from "@/lib/executeAction";
+
+export const createCategory = async (data: CategorySchema) => {
+  await executeAction({
+    actionFn: () =>
+      prisma.category.create({
+        data: {
+          name: data.name,
+        },
+      }),
+  });
+};
+
+export const updateCategory = async (data: CategorySchema) => {
+  if (data.action === "update") {
+    await executeAction({
+      actionFn: () =>
+        prisma.category.update({
+          where: { id: data.id },
+          data: {
+            name: data.name,
+          },
+        }),
+    });
+  }
+};
+
+export const deleteCategory = async (id: number) => {
+  await executeAction({
+    actionFn: () => prisma.category.delete({ where: { id } }),
+  });
+};
+
+export const getCategory = async (id: number): Promise<CategorySchema> => {
+  const res = await prisma.category.findFirst({ where: { id } });
+
+  return {
+    ...res,
+    action: "update",
+    name: res?.name ?? "",
+    id,
+  };
+};
